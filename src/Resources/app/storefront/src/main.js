73b44209eda713a132d0cfe677211677b83c59eb
weselben <bengottwaldi04@gmail.com>
2025-07-07 13:08:07 +0200

    "Refactor"

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,3 +1,13 @@
 import WslbnSimpleSubMenuPlugin from './simple-sub-menu/simple-sub-menu.plugin';
+import Viewport from 'src/helper/viewport.helper';
 
 const PluginManager = window.PluginManager;
+
+if (Viewport.is  ('lg')) {
+    if (window.features.V6_6_0_0) {
+        PluginManager.override('FlyoutMenu', () => import('./simple-sub-menu/simple-sub-menu.plugin'),  '[data-flyout-menu]');
+    }
+    else {
+        PluginManager.override('FlyoutMenu', WslbnSimpleSubMenu, '[data-flyout-menu]');
+    }
+}
